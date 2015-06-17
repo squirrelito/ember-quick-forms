2b6e18dbe2281080ad8ed83d2959e43379eedcc6
SquirreLito <g.budinov@gmail.com>
2015-06-18 02:15:05 +0300

    Initial setup

--- addon/components/qf-form.js
+++ addon/components/qf-form.js
@@ -11,6 +11,16 @@ export default Ember.Component.extend({
         var wrapper = this.get('wrapper');
         this.set('layout', Ember.Handlebars.compile('{{#quick-forms/' + wrapper + '/qf-form tagName=""}}{{yield}}{{/quick-forms/' + wrapper + '/qf-form}}'));
         this._super.apply(this, arguments);
+
+        this.set('model.backendValidationErrors', false);
+        this.get('model').addObserver('backendValidationErrors', (function(_this) {
+            return function() {
+                if (_this.get('model.backendValidationErrors')) {
+                    _this.set('showAllErrors', false);
+                    _this.set('showAllErrors', true);
+                }
+            };
+        })(this));
     },
     submit: function (e) {
         e.preventDefault();
@@ -18,8 +28,10 @@ export default Ember.Component.extend({
         if (this.get('for')) {
             if (!this.get('validate') || Ember.isNone(this.get('model.validate'))) {
                 this.set('validate', true);
+                this.set('model.backendValidationErrors', false);
                 this.sendAction('action', this.get('for'), this.get('model'));
             } else {
+                this.set('model.backendValidationErrors', false);
                 var promise = this.get('model').validate();
                 promise.then((function(_this) {
                     return function() {
