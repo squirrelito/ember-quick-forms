4cf5f9ba008a60ff980b879f52ab150a3b0acb0e
SquirreLito <g.budinov@gmail.com>
2015-10-01 23:40:53 +0300

    Ember 2.0 updates and blueprints

--- blueprints/ember-quick-forms/index.js
+++ blueprints/ember-quick-forms/index.js
@@ -1,12 +1,6 @@
 module.exports = {
   description: '',
   normalizeEntityName: function () {},
-  included: function (app) {
-    this._super.included(app);
-    app.import(app.bowerDirectory + '/ember/ember-template-compiler.js');
-    app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.min.css');
-    app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.min.js');
-  },
   afterInstall: function () {
     return this.addBowerPackagesToProject([
       {'name':'ember', 'target': '2.x'},
--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-quick-forms'
+  name: 'ember-quick-forms',
+  included: function (app) {
+    this._super.included(app);
+    app.import(app.bowerDirectory + '/ember/ember-template-compiler.js');
+    app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.min.css');
+    app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.min.js');
+  }
 };
